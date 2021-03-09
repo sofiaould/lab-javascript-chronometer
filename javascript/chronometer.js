674fddd5cb21e0ecf4458eab29a1757e68385590
sofiaould <sofiaouldabderrahmane@yahoo.fr>
2021-03-09 20:18:41 +0100

    done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,4 +1,4 @@
-class Chronometer {
+/*class Chronometer {
   constructor() {
   this.currentTime= 0;
    this.intervalId= 0;
@@ -11,21 +11,70 @@ class Chronometer {
     // ... your code goes here
   }
   getMinutes() {
-    // ... your code goes here
-  }
+      let getMinute = math.floor(this.currentTime/ 60);
+        
+      return getMinute // utiliser math.floor() pour arrondir 
+     }
+   
+
+ 
   getSeconds() {
+    let getSecond = this.currentTime / 1000;
+    return this.currentTime;
     // ... your code goes here
   }
   twoDigitsNumber() {
-    // ... your code goes here
+    // 1 creer une chaine de charactere 
+    //2 arrondir a deux chiffre avec .tofixed(2)
+  
   }
   stopClick() {
-    // ... your code goes here
+  clearInterval(this.intervalID);
+
   }
   resetClick() {
-    // ... your code goes here
+    
+    this.currentTime =0;
   }
   splitClick() {
     // ... your code goes here
+  }
+}
+*/
+
+class Chronometer {
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+  }
+  startClick(callback) {
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (callback) callback()
+    }, 1000)
+  }
+  getMinutes() {
+    return Math.floor(this.currentTime / 60)
+  }
+  getSeconds() {
+    return this.currentTime % 60; // rest of the division by 60
+  }
+  twoDigitsNumber(num) {
+    if (num < 10) {
+      return `0${num}`; // add a 0
+    } else {
+      return `${num}`;  // just convert to a string
+    }
+  }
+  stopClick() {
+    clearInterval(this.intervalId);
+  }
+  resetClick() {
+    this.currentTime = 0;
+  }
+  splitClick() {
+    const mm = this.twoDigitsNumber(this.getMinutes());
+    const ss = this.twoDigitsNumber(this.getSeconds());
+    return `${mm}:${ss}`;
   }
 }
